e7a249666ce3b5c8fa182a84e8816c3a2a6afd9c
holzmanj <jesseholzman96@gmail.com>
2018-11-08 14:58:49 -0500

    Added all of neural network code

---
+++ neuralnet/main.py
@@ -0,0 +1,300 @@
+import sys
+import os
+
+import numpy as np
+import tensorflow as tf
+
+from PIL import Image
+from src.config import Config
+from src.dataset import Dataset
+from src.model import StegoNet
+
+
+def random_image_batch(batch_size, img_size, num_channels):
+    '''
+    Arguments:
+        batch_size: Number of images to generate
+        img_size: Pixel height/width of image
+        num_channels: Channel depth of image
+    Returns:
+        batch: A [batch_size, img_size, img_size, num_channels]
+               array of (0., 1.) floating values
+    '''
+
+    # Generates random pixel vals
+    # batch = np.random.randint(0, 256, size=(
+    #     batch_size, img_size, img_size, num_channels)).astype(float)
+    # return batch / 255.
+
+    batch = np.random.randint(2, size=(
+        batch_size, img_size, img_size, num_channels)).astype(float)
+
+    return ((batch * 2) - 1)
+
+
+def random_boolean_batch(batch_size, n):
+    '''
+    Arguments:
+        batch_size: Number of arrays to return
+        n: Length in random boolean batch
+    Returns:
+        batch: A [batch_size, n] array of (-1./1.) boolean numbers
+    '''
+
+    batch = np.random.randint(2, size=(batch_size, n))
+    batch = (batch * 2) - 1
+    return batch.astype(float)
+
+
+def get_batch(batch_size, msg_size, key_size):
+    '''
+    Arguments:
+        batch_size: Number of messages and keys to generate
+        msg_size: Bit length of each message
+        key_size: Bit length of each key
+    Returns:
+        msg_batch: A [batch_size, msg_size] array of (-1./1.) boolean values
+        key_batch: A [batch_size, key_size] array of (-1./1.) boolean values
+    '''
+    msg_batch = random_boolean_batch(batch_size, msg_size)
+    key_batch = random_boolean_batch(batch_size, key_size)
+
+    return msg_batch, key_batch
+
+
+def nn_to_bin(batch):
+    return np.around(batch).astype(int)
+
+
+def eval(sess, cfg, dst, img_loss_op, bob_reconstruction_loss_op, n):
+    '''
+    Evaluates the current network on n batches of random examples
+
+    Arguments:
+        sess: The current tensorflow session
+        cfg: An instance of the Config class
+        dst: An instance of the Dataset class
+        eve_loss_op: Eve's loss tensor
+        bob_reconstruction_loss_op: Bob's reconstruction loss tensor
+        n: The number of iterations to run
+
+    Returns:
+        bob_loss: Bob's error rate
+        eve_loss: Eve's error rate
+    '''
+    bob_loss_total = 0
+    img_loss_total = 0
+    for _ in range(n):
+        ib = dst.get_batch(cfg.BATCH_SIZE, cfg.IMG_SIZE)
+        mb = random_image_batch(cfg.BATCH_SIZE, cfg.IMG_SIZE, 1)
+        # mb, kb = get_batch(cfg.BATCH_SIZE, cfg.MSG_SIZE, cfg.KEY_SIZE)
+        bl, il = sess.run(
+            [bob_reconstruction_loss_op, img_loss_op], feed_dict={'img_in:0': ib, 'msg_in:0': mb})
+        bob_loss_total += bl
+        img_loss_total += il
+    bob_loss = bob_loss_total / (n * cfg.BATCH_SIZE)
+    img_loss = img_loss_total / (n * cfg.BATCH_SIZE)
+    return bob_loss, img_loss
+
+
+def train(cfg, model_dir, train_set, val_set):
+    # Create folders for model weights and logging
+    save_dir = os.path.join(model_dir, "data")
+    log_dir = os.path.join(model_dir, "logs")
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+
+    # Create log file or resume previous
+    current_epoch = 0
+    log_file_name = os.path.join(log_dir, cfg.MODEL_NAME + "_log.csv")
+    if os.path.isfile(log_file_name):
+        with open(log_file_name, 'r') as log_file:
+            lines = log_file.readlines()
+            if len(lines) > 1:
+                current_epoch = int(lines[-1].split(',')[0])
+                print("Resuming from epoch %d..." % current_epoch)
+                log_file = open(log_file_name, 'a')
+    if current_epoch == 0:
+        log_file = open(os.path.join(
+            log_dir, cfg.MODEL_NAME + "_log.csv"), 'w')
+        log_file.write("epoch,bob_loss,eve_loss\n")
+
+    # Restore meta graph if loading previous model
+    if current_epoch != 0:
+        weight_file_name = cfg.MODEL_NAME + '_train-' + str(current_epoch)
+        meta_file_name = weight_file_name + '.meta'
+
+    with tf.Session() as sess:
+        if current_epoch != 0:
+            saver = tf.train.import_meta_graph(
+                os.path.join(save_dir, meta_file_name))
+            saver.restore(sess, tf.train.latest_checkpoint(save_dir))
+
+        else:
+            _ = StegoNet(cfg)
+            sess.run(tf.global_variables_initializer())
+            saver = tf.train.Saver()
+
+        # Get initial loss from starting weights
+        bob_loss, img_loss = eval(
+            sess, cfg, val_set, 'img_loss:0', 'bob_reconstruction_loss:0', 16)
+        if (current_epoch == 0):
+            log_file.write("0,%f,%f\n" % (bob_loss, img_loss))
+
+        print("Training for %d epochs..." % (cfg.NUM_EPOCHS))
+        for epoch in range(current_epoch, current_epoch + cfg.NUM_EPOCHS):
+            for _ in range(cfg.ITERS_PER_ACTOR * cfg.ALICE_MULTIPLIER):
+                ib = train_set.get_batch(cfg.BATCH_SIZE, cfg.IMG_SIZE)
+                mb = random_image_batch(cfg.BATCH_SIZE, cfg.IMG_SIZE, 1)
+                # mb, kb = get_batch(cfg.BATCH_SIZE, cfg.MSG_SIZE, cfg.KEY_SIZE)
+                sess.run('alice_bob_optimizer', feed_dict={
+                         'img_in:0': ib, 'msg_in:0': mb})
+            for _ in range(cfg.ITERS_PER_ACTOR * cfg.BOB_MULTIPLIER):
+                ib = train_set.get_batch(cfg.BATCH_SIZE, cfg.IMG_SIZE)
+                mb = random_image_batch(cfg.BATCH_SIZE, cfg.IMG_SIZE, 1)
+                # mb, kb = get_batch(cfg.BATCH_SIZE, cfg.MSG_SIZE, cfg.KEY_SIZE)
+                sess.run('bob_optimizer', feed_dict={
+                         'img_in:0': ib, 'msg_in:0': mb})
+            if (epoch + 1) % cfg.LOG_CHECKPOINT == 0:
+                bob_loss, img_loss = eval(
+                    sess, cfg, val_set, 'img_loss:0', 'bob_reconstruction_loss:0', 16)
+                log_file.write("%d,%f,%f\n" % (epoch + 1, bob_loss, img_loss))
+
+            # Display progress in console
+            prog = int(20. * (epoch - current_epoch + 1)/(cfg.NUM_EPOCHS))
+            prog_bar = "[%s%s%s]" % (
+                '=' * prog, ('=' if prog == 20 else '>'), '.' * (20 - prog),)
+            print("Epoch %06d/%06d - %s\tbob_loss: %f\timg_loss: %f" % (
+                epoch + 1, current_epoch + cfg.NUM_EPOCHS, prog_bar, bob_loss, img_loss), end="\r", flush=True)
+        print('\n')
+        save_name = cfg.MODEL_NAME + "_train"
+        saver.save(sess, os.path.join(save_dir, save_name),
+                   global_step=(current_epoch + cfg.NUM_EPOCHS))
+    log_file.close()
+
+
+# def run_tests(cfg):
+#     stego_net = StegoNet(cfg)
+#     init = tf.global_variables_initializer()
+
+#     # Test saving and loading models
+#     with tf.Session() as sess:
+#         sess.run(init)
+#         bob_loss_orig, eve_loss_orig = eval(sess, cfg, stego_net, 16)
+
+def production_test(cfg, model_dir, dst):
+    save_dir = os.path.join(model_dir, "data")
+    log_dir = os.path.join(model_dir, "logs")
+    results_dir = os.path.join(model_dir, "results")
+
+    if not os.path.exists(save_dir):
+        print("Error: Model \"%s\" does not exist" % cfg.MODEL_NAME)
+        return False
+
+    if not os.path.exists(results_dir):
+        os.mkdir(results_dir)
+
+    # Use log file to infer current epoch
+    current_epoch = 0
+    log_file_name = os.path.join(log_dir, cfg.MODEL_NAME + "_log.csv")
+    if os.path.isfile(log_file_name):
+        with open(log_file_name, 'r') as log_file:
+            lines = log_file.readlines()
+            if len(lines) > 1:
+                current_epoch = int(lines[-1].split(',')[0])
+                print("Current epoch: %d\n" % current_epoch)
+            else:
+                print("Error: Cannot infer epoch from log file...")
+                return False
+    else:
+        print("Error: No log file from which to infer epoch")
+        return False
+
+    # Restore saved weights
+    weight_file_name = cfg.MODEL_NAME + '_train-' + str(current_epoch)
+    meta_file_name = weight_file_name + '.meta'
+
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(
+            os.path.join(save_dir, meta_file_name))
+        saver.restore(sess, tf.train.latest_checkpoint(save_dir))
+
+        num_tests = 5000
+        bob_passed = 0
+        for test in range(num_tests):
+            #print("\nTest %d:" % (test + 1))
+            ib = dst.get_batch(1, cfg.IMG_SIZE)
+            mb = random_image_batch(1, cfg.IMG_SIZE, 1)
+            msg_bin = nn_to_bin((mb + 1.) / 2.)
+
+            image_norm = (ib * 255.).astype('uint8')[0]
+            image = Image.fromarray(image_norm, 'RGB')
+            # image.save(os.path.join(
+            #    results_dir, (str(test) + "_original.bmp")))
+
+            a_out = sess.run('alice_out:0', feed_dict={
+                'img_in:0': ib, 'msg_in:0': mb})
+
+            alice_out_norm = (a_out * 255.).astype('uint8')[0]
+            alice_out_img = Image.fromarray(alice_out_norm, 'RGB')
+            # alice_out_img.save(os.path.join(
+            #    results_dir, (str(test) + "_embedded.bmp")))
+
+            a_out *= 255.
+            a_out = a_out.astype(int)
+            a_out = a_out.astype(float)
+            a_out /= 255.
+
+            b_out = sess.run('bob_vars_1/bob_eval_out:0',
+                             feed_dict={'img_in:0': a_out})
+            bob_out_bin = nn_to_bin((b_out + 1.) / 2.)
+            #print("bob_out: ", bob_out_bin)
+
+            bob_missed_bits = sess.run(
+                tf.reduce_sum(tf.abs(msg_bin - bob_out_bin)))
+            if bob_missed_bits == 0:
+                bob_passed += 1
+            else:
+                image.save(os.path.join(
+                    results_dir, (str(test) + "_original.bmp")))
+                alice_out_img.save(os.path.join(
+                    results_dir, (str(test) + "_embedded.bmp")))
+
+            #print("Bob missed: ", bob_missed_bits)
+            # Display progress in console
+            prog = int(20. * (test + 1)/(num_tests))
+            prog_bar = "[%s%s%s]" % (
+                '=' * prog, ('=' if prog == 20 else '>'), '.' * (20 - prog),)
+            print("Test %06d/%06d - %s\tFailed: %d" % (
+                test + 1, num_tests, prog_bar, test - bob_passed + 1), end="\r", flush=True)
+
+        print("\nFinal Results:")
+        print("Bob recovered: [%d/%d]" % (bob_passed, num_tests))
+
+
+def main():
+    cfg = Config()
+    cfg.print_summary()
+
+    train_set = Dataset(cfg, "train")
+    val_set = Dataset(cfg, "val")
+
+    # Parse command line args
+
+    # Make dirs for model saving and logging
+    root_dir = os.path.abspath("")
+    model_dir = os.path.join(root_dir, "models")
+    if not os.path.exists(model_dir):
+        os.mkdir(model_dir)
+    model_dir = os.path.join(model_dir, cfg.MODEL_NAME)
+    if not os.path.exists(model_dir):
+        os.mkdir(model_dir)
+
+    #train(cfg, model_dir, train_set, val_set)
+    production_test(cfg, model_dir, val_set)
+
+
+if __name__ == '__main__':
+    main()
--- neuralnet/stegasaurus.py
+++ neuralnet/stegasaurus.py
@@ -1,10 +1,22 @@
+import sys
+import os
+
+import numpy as np
+import tensorflow as tf
+
+from PIL import Image
+from src.config import Config
+
 import cv2
 import numpy as np
 import hashlib
 import math
 import os
 
-FAIL_ON_BAD_HASH = True
+
+def nn_to_bin(batch):
+    return np.around(batch).astype(int)
+
 
 def generate_header(file_path):
     """
@@ -17,7 +29,8 @@ def generate_header(file_path):
     # 4 bytes for filesize
     filesize = os.path.getsize(file_path)
     if filesize >= 2**32:
-        raise Exception("File too big. Header currently only works for files up to 4GB.")
+        raise Exception(
+            "File too big. Header currently only works for files up to 4GB.")
     header_bytes = filesize.to_bytes(4, byteorder="big")
 
     # 16 bytes for hash
@@ -30,6 +43,7 @@ def generate_header(file_path):
     # pad header to be exactly 3 batches in size
     header_bytes = header_bytes + b'\0' * (384 - len(header_bytes))
     return header_bytes
+
 
 def parse_header(header_bytes):
     """
@@ -46,9 +60,11 @@ def parse_header(header_bytes):
         f_name = f_name.decode()
     except:
         print(f_name)
-        raise Exception("Content could not be retrieved from image. (File name could not be parsed.)")
+        raise Exception(
+            "Content could not be retrieved from image. (File name could not be parsed.)")
 
     return size, f_hash, f_name.strip()
+
 
 def bytes_to_batch(byte_str):
     """
@@ -56,11 +72,13 @@ def bytes_to_batch(byte_str):
     Bytes are expanded into bits and converted to {-1.0, 1.0} in place of {0, 1}.
     """
     if len(byte_str) != 128:
-        raise Exception("bytes_to_batch takes exactly 128 bytes (got %d)" % len(byte_str))
+        raise Exception(
+            "bytes_to_batch takes exactly 128 bytes (got %d)" % len(byte_str))
 
     # convert to list of 1024 bits
-    bit_list = lambda b: [int(i) for i in list(bin(b)[2:].zfill(8))]
-    flatten = lambda l: [item for sublist in l for item in sublist]
+    def bit_list(b): return [int(i) for i in list(bin(b)[2:].zfill(8))]
+
+    def flatten(l): return [item for sublist in l for item in sublist]
     bits = flatten([bit_list(b) for b in list(byte_str)])
 
     # convert from binary bits to -1.0 and 1.0 for neural network
@@ -75,6 +93,7 @@ def bytes_to_batch(byte_str):
             ptr += 1
     return batch
 
+
 def batch_to_bytes(batch):
     """
     The inverse of the bytes_to_batch function.  A 32x32x1 numpy array with values {-1.0, 1.0} is converted
@@ -84,7 +103,7 @@ def batch_to_bytes(batch):
         raise Exception("Got batch with invalid shape: %s" % str(batch.shape))
 
     # convert 1.0 and -1.0 values back to integer 1 and 0 bits
-    tobits = lambda b: 1 if b > 0.0 else 0
+    def tobits(b): return 1 if b > 0.0 else 0
     batch = np.vectorize(tobits)(batch)
 
     # extract bits from batch and turn into bytes
@@ -98,21 +117,17 @@ def batch_to_bytes(batch):
                 bits = ""
     return bytes(byte_vals)
 
-def file_to_batches(file_path, n=None):
+
+def file_to_batches(file_path):
     """
     Reads file from given path and converts it into nx32x32x1 batches for the neural network
     First three batches hold the header data (file name, file size, and hash).
-    If n is set, the batch set will be padded to n batches.
     """
     header_bytes = generate_header(file_path)
 
     filesize = os.path.getsize(file_path)
-    if n is None:
-        # 128 = number of bytes in 32x32 bitplane, 3 additional batches for header
-        n = math.ceil(filesize / 128) + 3
-    elif n < math.ceil(filesize / 128) + 3:
-        raise Exception("Value for n is too small. Batch set for %s would be %d batches long."
-                % (os.path.basename(file_path), math.ceil(filesize / 128) + 3))
+    # 128 = number of bytes in 32x32 bitplane, 3 additional batches for header
+    n = math.ceil(filesize / 128) + 3
     batches = np.zeros((n, 32, 32, 1))
 
     with open(file_path, "rb") as file:
@@ -120,7 +135,7 @@ def file_to_batches(file_path, n=None):
         for i in range(3):
             batches[i] = bytes_to_batch(header_bytes[128*i:128*(i+1)])
 
-        # convert file to batches
+        # convert image to batches
         for batch_no in range(3, n):
             buf = file.read(128)
 
@@ -130,6 +145,7 @@ def file_to_batches(file_path, n=None):
 
             batches[batch_no] = bytes_to_batch(buf)
     return batches
+
 
 def batches_to_file(batches, output_dir):
     """
@@ -143,7 +159,8 @@ def batches_to_file(batches, output_dir):
     file_size, header_hash, file_name = parse_header(header_bytes)
 
     if file_size > (batches.shape[0] - 3) * 128:
-        raise Exception("Content could not be retrieved from image. File size in header is too large.")
+        raise Exception(
+            "Content could not be retrieved from image. File size in header is too large.")
 
     # get file data from the rest of the batches
     file_bytes = b""
@@ -156,15 +173,15 @@ def batches_to_file(batches, output_dir):
     # verify hash
     file_hash = hashlib.md5(file_bytes).digest()
     if file_hash != header_hash:
-        if FAIL_ON_BAD_HASH:
-            raise Exception("Content could not be retrieved from image. Hashes do not match.")
-        else:
-            print("Data was not retrieved successfully. Hash verification failed.")
+        print("Warning: Hashes do not match")
+        # raise Exception(
+        #    "Content could not be retrieved from image. Hashes do not match.")
 
     # save file
     output_path = os.path.join(output_dir, file_name)
     with open(output_path, "wb") as file_out:
         file_out.write(file_bytes)
+
 
 def image_to_batches(img):
     """
@@ -184,6 +201,7 @@ def image_to_batches(img):
     # convert pixels from byte values into floats
     batches = np.vectorize(lambda b: b / 255)(batches)
     return batches
+
 
 def batches_to_image(batches, img):
     """
@@ -219,35 +237,126 @@ def batches_to_image(batches, img):
 #                                                             #
 ###############################################################
 
-def insert(img_path, file_path):
+
+def insert(cfg, sess, img_path, file_path):
     """
     Inserts the file at file_path into the image at img_path using the neural network.
     Returns the image as an opencv numpy array.  Can be saved with: cv2.imwrite(path, image)
     """
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-    img_batches = img_to_batches(img)
-    file_batches = file_to_batches(file_path, n=img_batches.size[0])
+    img_batches = image_to_batches(img)
+    file_batches = file_to_batches(file_path)
 
-    batches_out = None
-    #
-    #   GIVE BATCHES TO NEURAL NETWORK HERE
-    #   batches_out = Alice.insert(img_batches, file_batches)
-    #
+    print(tf.shape(img_batches))
+    print(tf.shape(file_batches))
+
+    batches_out = sess.run('alice_out:0', feed_dict={
+        'img_in:0': img_batches, 'msg_in:0': file_batches})
+
     img_out = batches_to_image(batches_out, img)
     return img_out
 
-def extract(img_path, output_dir):
+
+def extract(cfg, sess, img_path, output_dir):
     """
     Extracts a content file from the image at img_path using the neural network.
     On success the file is saved to output_dir (file name comes from header).
     """
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-    img_batches = img_to_batches(img)
+    img_batches = image_to_batches(img)
 
-    batches_out = None
-    #
-    #   GIVE BATCHES TO NEURAL NETWORK HERE
-    #   batches_out = Bob.extract(img_batches)
-    #
+    batches_out = sess.run('bob_vars_1/bob_eval_out:0',
+                           feed_dict={'img_in:0': img_batches})
+
     batches_to_file(batches_out, output_dir)
-    
+
+
+def main():
+    cfg = Config()
+
+    # Prepare directories
+    root_dir = os.path.abspath("")
+    model_dir = os.path.join(root_dir, "models")
+    model_dir = os.path.join(model_dir, cfg.MODEL_NAME)
+    if not os.path.exists(model_dir):
+        print("Error: No model exists at %s" % model_dir)
+        return False
+
+    target_dir = os.path.join(root_dir, "production")
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    in_dir = os.path.join(target_dir, "input")
+    out_dir = os.path.join(target_dir, "output")
+    if not os.path.exists(in_dir):
+        os.mkdir(in_dir)
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+    # Load model
+    save_dir = os.path.join(model_dir, "data")
+    log_dir = os.path.join(model_dir, "logs")
+
+    # Use log file to infer current epoch
+    current_epoch = 0
+    log_file_name = os.path.join(log_dir, cfg.MODEL_NAME + "_log.csv")
+    if os.path.isfile(log_file_name):
+        with open(log_file_name, 'r') as log_file:
+            lines = log_file.readlines()
+            if len(lines) > 1:
+                current_epoch = int(lines[-1].split(',')[0])
+                print("Current epoch: %d\n" % current_epoch)
+            else:
+                print("Error: Cannot infer epoch from log file...")
+                return False
+    else:
+        print("Error: No log file from which to infer epoch")
+        return False
+
+    # Restore saved weights
+    weight_file_name = cfg.MODEL_NAME + '_train-' + str(current_epoch)
+    meta_file_name = weight_file_name + '.meta'
+
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(
+            os.path.join(save_dir, meta_file_name))
+        saver.restore(sess, tf.train.latest_checkpoint(save_dir))
+
+        repeat = True
+        while repeat:
+            mode = None
+            if mode != 'e' and mode != 'd':
+                mode = input("Encrypt or Decrypt? (E/D): ").lower()
+            if mode == 'e':
+                # Embed using Alice net
+                user_in = input("Please enter name of vessel image: ")
+                vessel_img_path = os.path.join(in_dir, user_in)
+
+                user_in = input("Please enter file to embed: ")
+                target_file_path = os.path.join(in_dir, user_in)
+
+                if os.path.exists(vessel_img_path) and os.path.exists(target_file_path):
+                    img = insert(cfg, sess, vessel_img_path, target_file_path)
+                    cv2.imwrite(vessel_img_path[:-4] + '_embedded.bmp', img)
+                else:
+                    if not os.path.exists(vessel_img_path):
+                        print("Error: No image at %s" % vessel_img_path)
+                    if not os.path.exists(target_file_path):
+                        print("Error: No file at %s" % target_file_path)
+            else:
+                # Extract using Bob net
+                user_in = input("Please enter name of embedded image: ")
+                embedded_img_path = os.path.join(in_dir, user_in)
+                if os.path.exists(embedded_img_path):
+                    extract(cfg, sess, embedded_img_path, out_dir)
+                else:
+                    print("Error: No file at %s" % embedded_img_path)
+
+            user_in = None
+            if user_in != 'y' and user_in != 'n':
+                user_in = input("Repeat? (Y/N): ").lower()
+            if user_in == 'n':
+                repeat = False
+
+
+if __name__ == '__main__':
+    main()
