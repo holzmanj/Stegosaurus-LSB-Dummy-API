3ffc5ff1e93fc7adb8efc028cc7fa8aa1c0af9f3
holzmanj <jesseholzman96@gmail.com>
2018-12-06 17:30:06 -0500

    removed print statements

--- app.py
+++ app.py
@@ -235,7 +235,6 @@ class Insert(Resource):
                 pix_w, pix_h, nn.format_capacity(os.stat(file_name).st_size)))
             nn_time_0 = time.time()
             key = args["key"].strip("\"\n")
-            print(key)
             key = bytes.fromhex(key)
             nn.insert(cfg, sess, app.logger, image_fname, file_name, key, output_fpath)
             nn_time_diff = time.time() - nn_time_0
@@ -304,7 +303,6 @@ class Extract(Resource):
             app.logger.info("Calling neural network extract for %s image." %
                 nn.format_capacity(os.stat(image_fname).st_size))
             nn_time_0 = time.time()
-            print(args["key"].strip("\"\n"))
             key = bytes.fromhex(args["key"].strip("\"\n"))
             file_out = nn.extract(cfg, sess, app.logger, image_fname, key, files_dir)
             nn_time_diff = time.time() - nn_time_0
